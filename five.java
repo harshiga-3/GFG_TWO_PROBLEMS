122da06fbf4615db542bc683f87b52b9b25cee94
harsh <harshigadevi@gmail.com>
2026-02-15 23:35:24 +0530

    alternate element

---
+++ five.java
@@ -0,0 +1,13 @@
+class Solution {
+    public ArrayList<Integer> getAlternates(int arr[]) {
+        // Code Here
+        ArrayList<Integer>a=new ArrayList<>();
+        
+        for(int i=0;i<arr.length;i+=2)
+        {
+            a.add(arr[i]);
+        }
+        
+        return a;
+    }
+}
