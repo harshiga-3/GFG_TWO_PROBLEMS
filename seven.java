fe6bf77661a66e586a225dfd40f244e9180fe02d
harsh <harshigadevi@gmail.com>
2026-02-16 21:24:31 +0530

    meeting rooms

---
+++ seven.java
@@ -0,0 +1,15 @@
+class Solution {
+    static boolean canAttend(int[][] arr) {
+        // code here
+        Arrays.sort(arr,(a,b)->a[0]-b[0]);
+        for(int i=1;i<arr.length;i++)
+        {
+            if(arr[i][0] < arr[i-1][1])
+            {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
