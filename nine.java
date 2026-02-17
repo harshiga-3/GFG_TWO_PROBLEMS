f29f738d948df53173c796f2a0c1980d1be60f11
harsh <harshigadevi@gmail.com>
2026-02-17 20:10:56 +0530

    rearrange the LL

---
+++ nine.java
@@ -0,0 +1,52 @@
+/*
+class Node {
+    int data;
+    Node next;
+
+    Node(int d)
+    {
+        data = d;
+        next = null;
+    }
+}*/
+
+class Solution {
+    public Node segregate(Node head) {
+        // code here
+        Node zeroHead=new Node(-1);
+        Node oneHead=new Node(-1);
+        Node twoHead=new Node(-1);
+        
+        Node zero=zeroHead;
+        Node one=oneHead;
+        Node two=twoHead;
+        
+        Node temp=head;
+        while(temp!=null)
+        {
+           if(temp.data==0)
+           {
+               zero.next=temp;
+               zero=zero.next;
+           }
+           else if(temp.data==1)
+           {
+               one.next=temp;
+               one=one.next;
+           }
+           else if(temp.data==2)
+           {
+               two.next=temp;
+               two=two.next;
+           }
+           
+           temp=temp.next;
+        }
+        
+        zero.next=oneHead.next!=null?oneHead.next:twoHead.next;
+        one.next=twoHead.next;
+        two.next=null;
+        
+        return zeroHead.next;
+    }
+}
