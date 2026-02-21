9adcae7030dfe0bd151545f0b6e21be6ff60e7a7
harsh <harshigadevi@gmail.com>
2026-02-21 22:46:29 +0530

    arrays

---
+++ code13.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int hIndex(int[] c) {
+        // code here
+        Arrays.sort(c);
+       int  n=c.length;
+for(int i=0;i<c.length;i++)
+{
+    if(c[i]>=n-i)
+    {
+        return n-i;
+    }
+}
+return 0;
+    }
+}
