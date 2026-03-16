e5346ce6a33d67beb754542f297a296593e6f105
harsh <harshigadevi@gmail.com>
2026-03-16 22:02:03 +0530

    palindrome array

---
+++ code36.java
@@ -0,0 +1,18 @@
+
+class Solution {
+    public static boolean isPerfect(int[] arr) {
+        // code here
+        int i=0;
+        int j=arr.length-1;
+        while(i<j)
+        {
+            if(arr[i]!=arr[j])
+            {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
